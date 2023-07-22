5e4e9adbb4bfde5b7d283e0136783984b28e8b2a
Lucas Souza <lucaseduardoid1@gmail.com>
2023-07-22 14:50:27 -0400

    feat: add reminders list view

---
+++ Today/ListViewController/ReminderListViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  Today
+//
+//  Created by Lucas Souza on 13/06/23.
+//
+
+import UIKit
+
+class ReminderListViewController: UICollectionViewController {
+    var dataSource: DataSource!
+    var reminders: [Reminder] = Reminder.sampleData
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let listLayout = listLayout()
+        collectionView.collectionViewLayout = listLayout
+        
+        let cellRegistration = UICollectionView.CellRegistration(handler: cellRegistrationHandler)
+        
+        dataSource = DataSource(collectionView: collectionView) {
+            (collectionView: UICollectionView, indexPath: IndexPath, itemIdentifier: Reminder.ID) in
+            let cell = collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: itemIdentifier)
+            
+            return cell
+        }
+        
+        var snapshot = Snapshot()
+        snapshot.appendSections([0])
+        snapshot.appendItems(reminders.map { $0.id })
+        dataSource.apply(snapshot)
+        
+        collectionView.dataSource = dataSource
+    }
+
+    private func listLayout() -> UICollectionViewCompositionalLayout {
+        var listConfiguration = UICollectionLayoutListConfiguration(appearance: .grouped)
+        listConfiguration.showsSeparators = false
+        listConfiguration.backgroundColor = .clear
+        
+        return UICollectionViewCompositionalLayout.list(using: listConfiguration)
+    }
+}
