6507234132a339a4be9ae25747b918e7a683f3bf
Lucas Souza <lucaseduardoid1@gmail.com>
2023-07-24 16:07:36 -0400

    style: style the navigation bar

--- Today/DetailViewController/ReminderViewController.swift
+++ Today/DetailViewController/ReminderViewController.swift
@@ -38,6 +38,11 @@ class ReminderViewController: UICollectionViewController {
             return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: row)
         }
         
+        if #available(iOS 16, *) {
+            navigationItem.style = .navigator
+        }
+        navigationItem.title = NSLocalizedString("Reminder", comment: "Reminder view controller title")
+        
         updateSnapshot()
     }
     
